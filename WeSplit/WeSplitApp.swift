724d90143668eee6b3340fa9f0e90f0461b7c055
Shiva Krishna Gopannagari <gopannagarishiva@gmail.com>
2026-09-11 22:58:07 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Shiva G on 11/09/26.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
