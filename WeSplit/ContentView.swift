1638150f9a0debf9eaf0f2f4a9497073c6e7ee9a
Shiva Krishna Gopannagari <gopannagarishiva@gmail.com>
2026-09-15 23:38:29 +0530

    Implemented conditional modifier based tip percentage

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -57,6 +57,7 @@ struct ContentView: View {
                 
                 Section("Total amount including tip") {
                     Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .foregroundColor(tipPercentage == 0 ? .red : .black)
                 }
                 
                 Section("Amount per person") {
